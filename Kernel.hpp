c5fcf5a78ce746b870158f355ecf2d83cf4d2d7d
Skylar Moore <nickgray0@gmail.com>
2016-03-21 05:28:59 -0700

    whatever..

---
+++ Kernel.hpp
@@ -0,0 +1,188 @@
+#ifndef KERNEL_HPP
+#define KERNEL_HPP
+
+#include <vector>
+#include <algorithm>
+#include "VapourSynth.h"
+#include "VSHelper.h"
+using namespace std;
+
+template<typename T>
+static inline T _abs(T val) {
+	return (val > 0) ? val : -val;
+}
+
+static inline void _clamp(long &val, int bits) {
+	val = max(min(val, (1l << bits) - 1l), 0l);
+}
+
+template<typename T1, typename T2>
+static void _pad(const T1 *&srcp, int &src_stride, T1 *&padp, int pad_stride, int h, int w) {
+	T1 *padtmp = padp;
+	for (int y = 1; y < h + 1; ++y) {
+		padp += pad_stride;
+		for (int x = 1; x < w + 1; ++x)
+			padp[x] = srcp[x - 1];
+		srcp += src_stride;
+	}
+	srcp = padtmp + pad_stride + 1;
+	src_stride = pad_stride;
+	padp += pad_stride;
+	for (int x = 1; x < w + 1; ++x)
+		padp[x] = padp[x - pad_stride];
+	padp = padtmp;
+	for (int x = 1; x < w + 1; ++x)
+		padp[x] = padp[x + pad_stride];
+	for (int y = 1; y < h + 1; ++y)
+		padp[y*pad_stride] = padp[y*pad_stride + 1];
+	padp = padp + pad_stride - 1;
+	for (int y = 1; y < h + 1; ++y)
+		padp[y*pad_stride] = padp[y*pad_stride - 1];
+	padp = padtmp;
+	*padp = static_cast<T1>((static_cast<T2>(padp[1]) + padp[pad_stride]) / 2);
+	padp = padp + pad_stride - 1;
+	*padp = static_cast<T1>((static_cast<T2>(*(padp - 1)) + padp[pad_stride]) / 2);
+	padp += (h + 1)*pad_stride;
+	*padp = static_cast<T1>((static_cast<T2>(*(padp - 1)) + *(padp - pad_stride)) / 2);
+	padp = padtmp + (h + 1)*pad_stride;
+	*padp = static_cast<T1>((static_cast<T2>(padp[1]) + *(padp - pad_stride)) / 2);
+	padp = nullptr;
+}
+
+static void _gauss_3x3(float *&dstp, vector<float> &a, int x) {
+	dstp[x] = static_cast<float>(
+		(4. * a[0] +
+			2. * (static_cast<double>(a[2]) + a[4] + a[5] + a[7]) +
+			a[1] + a[3] + a[6] + a[8]) / 16.);
+}
+
+static void _gauss_3x3(uint16_t *&dstp, vector<uint16_t> &a, int x) {
+	long val = ((a[0] << 2l) +
+		((static_cast<uint32_t>(a[2]) + a[4] + a[5] + a[7]) << 1l) +
+		a[1] + a[3] + a[6] + a[8] + 8l) >> 4l;
+	_clamp(val, 16);
+	dstp[x] = static_cast<uint16_t>(val);
+}
+
+static void _gauss_3x3(uint8_t *&dstp, vector<uint8_t> &a, int x) {
+	long val = ((a[0] << 2l) +
+		((static_cast<uint32_t>(a[2]) + a[4] + a[5] + a[7]) << 1l) +
+		a[1] + a[3] + a[6] + a[8] + 8l) >> 4l;
+	_clamp(val, 8);
+	dstp[x] = static_cast<uint8_t>(val);
+}
+
+template<typename T>
+static void _median_3x3(T *&dstp, vector<T> &a, int x) {
+	nth_element(a.begin(), a.begin() + a.size() / 2, a.end());
+	dstp[x] = a[a.size() / 2];
+}
+
+template<typename T>
+static void _process_plane_3x3(const T *&srcp, int src_stride, T *&dstp, int dst_stride, int h, int w, bool gauss) {
+	const T *srctmp = srcp;
+	T *dsttmp = dstp;
+	void(*_func)(T *&, vector<T> &, int) = nullptr;
+	if (gauss)
+		_func = _gauss_3x3;
+	else
+		_func = _median_3x3;
+	for (int y = 0; y < h; ++y) {
+		for (int x = 0; x < w; ++x) {
+			vector<T> a{ *(srcp + x),
+				*(srcp + x - src_stride - 1), *(srcp + x - src_stride), *(srcp + x - src_stride + 1),
+				*(srcp + x - 1), *(srcp + x + 1),
+				*(srcp + x + src_stride - 1), *(srcp + x + src_stride), *(srcp + x + src_stride + 1) };
+			_func(dstp, a, x);
+		}
+		dstp += dst_stride;
+		srcp += src_stride;
+	}
+	srcp = srctmp;
+	dstp = dsttmp;
+}
+
+static inline void _reverse_dif(const float *&srcp, float *&dstp, int x, double _amp) {
+	dstp[x] = static_cast<float>((1. + _amp) * srcp[x] - dstp[x] * _amp);
+}
+
+static inline void _reverse_dif(const uint16_t *&srcp, uint16_t *&dstp, int x, double _amp) {
+	long val = static_cast<long>((1. + _amp) * srcp[x] - dstp[x] * _amp);
+	_clamp(val, sizeof(uint16_t) << 3);
+	dstp[x] = static_cast<uint16_t>(val);
+}
+
+static inline void _reverse_dif(const uint8_t *&srcp, uint8_t *&dstp, int x, double _amp) {
+	long val = static_cast<long>((1. + _amp) * srcp[x] - dstp[x] * _amp);
+	_clamp(val, sizeof(uint8_t) << 3);
+	dstp[x] = static_cast<uint8_t>(val);
+}
+
+template<typename T1, typename T2>
+static inline void _min_dif(const T1 *&srcp, T1 *&dstp, T1 *&dstp_a, T1 *&dstp_b, int x, double _amp) {
+	T2 _dif_a = _abs(static_cast<T2>(dstp_a[x]) - srcp[x]);
+	T2 _dif_b = _abs(static_cast<T2>(dstp_b[x]) - srcp[x]);
+	dstp[x] = (_dif_a > _dif_b) ? dstp_b[x] : dstp_a[x];
+	_reverse_dif(srcp, dstp, x, _amp);
+}
+
+template<typename T1, typename T2>
+static void _apply_dif(const T1 *&srcp, int src_stride, T1 *&dstp, int dst_stride, int h, int w, T1 *&dstp_a, T1 *&dstp_b, double _amp, int mode) {
+	const T1 *srctmp = srcp;
+	T1 *dsttmp = dstp;
+	T1 *dst_atmp = dstp_a;
+	T1 *dst_btmp = dstp_b;
+	for (int y = 0; y < h; ++y) {
+		for (int x = 0; x < w; ++x) {
+			if (!mode)
+				dstp[x] = srcp[x];
+			else if (mode == 1) {
+				dstp[x] = dstp_a[x];
+				_reverse_dif(srcp, dstp, x, _amp);
+			}
+			else if (mode == 2) {
+				dstp[x] = dstp_b[x];
+				_reverse_dif(srcp, dstp, x, _amp);
+			}
+			else
+				_min_dif<T1, T2>(srcp, dstp, dstp_a, dstp_b, x, _amp);
+		}
+		dstp += dst_stride;
+		srcp += src_stride;
+		dstp_a += dst_stride;
+		dstp_b += dst_stride;
+	}
+	srcp = srctmp;
+	dstp = dsttmp;
+	dstp_a = dst_atmp;
+	dstp_b = dst_btmp;
+}
+
+template<typename T1, typename T2>
+static inline void _do_it_(const VSAPI *&vsapi, const VSFrameRef *&src, int src_stride, VSFrameRef *&dst, int dst_stride, VSFrameRef *&pad, int pad_stride, int h, int w, VSFrameRef *&dst_a, VSFrameRef *&dst_b, int plane, double _amp, int mode) {
+	const T1 *srcp = reinterpret_cast<const T1 *>(vsapi->getReadPtr(src, plane));
+	T1 *padp = reinterpret_cast<T1 *>(vsapi->getWritePtr(pad, plane));
+	T1 *dstp = reinterpret_cast<T1 *>(vsapi->getWritePtr(dst, plane));
+	T1 *dstp_a = reinterpret_cast<T1 *>(vsapi->getWritePtr(dst_a, plane));
+	T1 *dstp_b = reinterpret_cast<T1 *>(vsapi->getWritePtr(dst_b, plane));
+	if (!mode)
+		_apply_dif<T1, T2>(srcp, src_stride, dstp, dst_stride, h, w, dstp_a, dstp_b, _amp, mode);
+	else if (mode == 1) {
+		_pad<T1, T2>(srcp, src_stride, padp, pad_stride, h, w);
+		_process_plane_3x3(srcp, src_stride, dstp_a, dst_stride, h, w, true);
+		_apply_dif<T1, T2>(srcp, src_stride, dstp, dst_stride, h, w, dstp_a, dstp_b, _amp, mode);
+	}
+	else if (mode == 2) {
+		_pad<T1, T2>(srcp, src_stride, padp, pad_stride, h, w);
+		_process_plane_3x3(srcp, src_stride, dstp_b, dst_stride, h, w, false);
+		_apply_dif<T1, T2>(srcp, src_stride, dstp, dst_stride, h, w, dstp_a, dstp_b, _amp, mode);
+	}
+	else {
+		_pad<T1, T2>(srcp, src_stride, padp, pad_stride, h, w);
+		_process_plane_3x3(srcp, src_stride, dstp_a, dst_stride, h, w, true);
+		_process_plane_3x3(srcp, src_stride, dstp_b, dst_stride, h, w, false);
+		_apply_dif<T1, T2>(srcp, src_stride, dstp, dst_stride, h, w, dstp_a, dstp_b, _amp, mode);
+	}
+}
+
+#endif
---
+++ Source.cpp
@@ -0,0 +1,122 @@
+#include <cstdlib>
+#include "Kernel.hpp"
+
+struct MinSRPData {
+	VSNodeRef *node;
+	const VSVideoInfo *vi;
+	double str[3];
+	int mode[3];
+};
+
+static void VS_CC minsrpInit(VSMap *in, VSMap *out, void **instanceData, VSNode *node, VSCore *core, const VSAPI *vsapi) {
+	MinSRPData *d = reinterpret_cast<MinSRPData *>(*instanceData);
+	vsapi->setVideoInfo(d->vi, 1, node);
+}
+
+static const VSFrameRef *VS_CC minsrpGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
+	MinSRPData *d = reinterpret_cast<MinSRPData *>(*instanceData);
+	d->vi = vsapi->getVideoInfo(d->node);
+	if (activationReason == arInitial) {
+		vsapi->requestFrameFilter(n, d->node, frameCtx);
+	}
+	else if (activationReason == arAllFramesReady) {
+		const VSFrameRef *src = vsapi->getFrameFilter(n, d->node, frameCtx);
+		const VSFormat *fi = d->vi->format;
+		int height = vsapi->getFrameHeight(src, 0);
+		int width = vsapi->getFrameWidth(src, 0);
+		VSFrameRef *dst_a = vsapi->newVideoFrame(fi, width, height, src, core);
+		VSFrameRef *dst_b = vsapi->newVideoFrame(fi, width, height, src, core);
+		VSFrameRef *dst = vsapi->newVideoFrame(fi, width, height, src, core);
+		VSFrameRef *pad = vsapi->newVideoFrame(fi, width + 2, height + 2, src, core);
+		int plane;
+		for (plane = 0; plane < fi->numPlanes; ++plane) {
+			int src_stride = vsapi->getStride(src, plane) / d->vi->format->bytesPerSample;
+			int dst_stride = vsapi->getStride(dst, plane) / d->vi->format->bytesPerSample;
+			int pad_stride = vsapi->getStride(pad, plane) / d->vi->format->bytesPerSample;
+			int h = vsapi->getFrameHeight(src, plane);
+			int w = vsapi->getFrameWidth(src, plane);
+			if (d->vi->format->bytesPerSample == 4)
+				_do_it_<float, double>(vsapi, src, src_stride, dst, dst_stride, pad, pad_stride, h, w, dst_a, dst_b, plane, d->str[plane], d->mode[plane]);
+			else if (d->vi->format->bytesPerSample > 1 && d->vi->format->bytesPerSample < 4)
+				_do_it_<uint16_t, int32_t>(vsapi, src, src_stride, dst, dst_stride, pad, pad_stride, h, w, dst_a, dst_b, plane, d->str[plane], d->mode[plane]);
+			else
+				_do_it_<uint8_t, int32_t>(vsapi, src, src_stride, dst, dst_stride, pad, pad_stride, h, w, dst_a, dst_b, plane, d->str[plane], d->mode[plane]);
+		}
+		vsapi->freeFrame(src);
+		vsapi->freeFrame(pad);
+		vsapi->freeFrame(dst_a);
+		vsapi->freeFrame(dst_b);
+		return dst;
+	}
+	return nullptr;
+}
+
+static void VS_CC minsrpFree(void *instanceData, VSCore *core, const VSAPI *vsapi) {
+	MinSRPData *d = reinterpret_cast<MinSRPData *>(instanceData);
+	vsapi->freeNode(d->node);
+	delete d;
+}
+
+static void VS_CC minsrpCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
+	MinSRPData d;
+	MinSRPData *data = nullptr;
+	d.node = vsapi->propGetNode(in, "clip", 0, 0);
+	d.vi = vsapi->getVideoInfo(d.node);
+	const int m = vsapi->propNumElements(in, "mode");
+	const int n = vsapi->propNumElements(in, "str");
+	if (m > d.vi->format->numPlanes) {
+		vsapi->setError(out, "MinSRP: number of modes specified must be equal to or fewer than the number of input planes");
+		vsapi->freeNode(d.node);
+		return;
+	}
+	if (n > d.vi->format->numPlanes) {
+		vsapi->setError(out, "MinSRP: number of the specified elements in str array must be equal to or fewer than the number of input planes");
+		vsapi->freeNode(d.node);
+		return;
+	}
+	for (int i = 0; i < 3; ++i) {
+		if (m == -1)
+			d.mode[0] = d.mode[1] = d.mode[2] = 3;
+		else
+			if (i < m) {
+				d.mode[i] = int64ToIntS(vsapi->propGetInt(in, "mode", i, nullptr));
+				if (d.mode[i] < 0 || d.mode[i] > 3) {
+					vsapi->setError(out, "MinSRP: invalid mode specified, only modes 0-3 supported");
+					vsapi->freeNode(d.node);
+					return;
+				}
+			}
+			else
+				d.mode[i] = d.mode[i - 1];
+		if (n == -1)
+			d.str[0] = d.str[1] = d.str[2] = 1.;
+		else
+			if (i < n)
+				d.str[i] = vsapi->propGetFloat(in, "str", i, nullptr);
+			else
+				d.str[i] = d.str[i - 1];
+		const int height = d.vi->height >> (i ? d.vi->format->subSamplingH : 0);
+		if (d.mode[i] && height < 3) {
+			vsapi->setError(out, "MinSRP: corresponding plane's height must be greater than or equal to 3");
+			vsapi->freeNode(d.node);
+			return;
+		}
+	}
+	if (!isConstantFormat(d.vi)) {
+		vsapi->setError(out, "MinSRP: only input with constant format supported");
+		vsapi->freeNode(d.node);
+		return;
+	}
+	data = new MinSRPData;
+	*data = d;
+	vsapi->createFilter(in, out, "MinSRP", minsrpInit, minsrpGetFrame, minsrpFree, fmParallel, 0, data, core);
+}
+
+VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
+	configFunc("com.sharpeners.minsrp", "minsrp", "VapourSynth MinSRP Filter", VAPOURSYNTH_API_VERSION, 1, plugin);
+	registerFunc("Sharp",
+		"clip:clip;"
+		"str:float[]:opt;"
+		"mode:int[]:opt;",
+		minsrpCreate, 0, plugin);
+}
